b5428bce9d5311df8f2ab36cdffee35131bcc4a7
Brian Lam <briantlam132@gmail.com>
2021-06-08 17:24:29 -0700

    make test class public

--- src/test/java/com/example/proj3/Proj3ApplicationTests.java
+++ src/test/java/com/example/proj3/Proj3ApplicationTests.java
@@ -3,7 +3,7 @@ package com.example.proj3;
 import org.junit.Assert;
 import org.junit.Test;
 
-class Proj3ApplicationTests {
+public class Proj3ApplicationTests {
 
 	@Test
 	public void printTest() {
